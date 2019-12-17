d44bbbdc8342c1b535217f88e33dfd386a0e9a7e
Jan Miksovsky <jmiksovsky@salesforce.com>
2019-12-17 13:15:05 -0800

    Play with DropdownList.

---
+++ src/DropdownList.js
@@ -0,0 +1,37 @@
+import * as internal from "../node_modules/elix/src/internal.js";
+import * as template from "../node_modules/elix/src/template.js";
+import DropdownList from "../node_modules/elix/src/DropdownList.js";
+import XButton from './Button.js';
+import XMenu from './Menu.js';
+import XPopup from "./Popup.js";
+
+export default class XDropdownList extends DropdownList {
+  get [internal.defaultState]() {
+    return Object.assign(super[internal.defaultState], {
+      horizontalAlign: 'stretch',
+      menuPartType: XMenu,
+      popupPartType: XPopup,
+      sourcePartType: XButton
+    });
+  }
+
+  get [internal.template]() {
+    return template.concat(
+      super[internal.template],
+      template.html`
+        <style>
+          /* slids-input */
+          :host {
+            height: 1.875rem;
+          }
+
+          #source {
+            padding: 0 0.75rem;
+          }
+        </style>
+      `
+    );
+  }
+}
+
+customElements.define("x-dropdown-list", XDropdownList);
