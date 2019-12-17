c11ca7f1ef8213d5f8d07812b896edb7dd251e2a
Jan Miksovsky <jmiksovsky@salesforce.com>
2019-12-16 16:26:53 -0800

    Use SLDS play/pause icons.

--- src/CarouselSlideshow.js
+++ src/CarouselSlideshow.js
@@ -1,3 +1,4 @@
+import './Button.js';
 import * as internal from "../node_modules/elix/src/internal.js";
 import * as template from "../node_modules/elix/src/template.js";
 import CarouselSlideshow from "../node_modules/elix/src/CarouselSlideshow.js";
@@ -29,7 +30,8 @@ export default class XCarouselSlideshow extends CarouselSlideshow {
     super[internal.render](changed);
     if (changed.playing) {
       const { playing } = this[internal.state];
-      this[internal.ids].playButton.textContent = playing ? '||' : '>';
+      this[internal.ids].playIcon.style.display = playing ? 'none' : 'block';
+      this[internal.ids].pauseIcon.style.display = playing ? 'block' : 'none';
     }
   }
 
@@ -41,7 +43,10 @@ export default class XCarouselSlideshow extends CarouselSlideshow {
     const explorerContainer = base.content.getElementById('explorerContainer');
     if (explorerContainer) {
       const playButtonTemplate = template.html`
-        <button id="playButton"></button>
+        <x-button id="playButton">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" id="playIcon" viewBox="0 0 52 52"><path d="m8 43.7v-35.4c0-1 1.3-1.7 2.2-0.9l33.2 17.3c0.8 0.6 0.8 1.9 0 2.5l-33.2 17.5c-0.9 0.7-2.2 0.1-2.2-1z"></path></svg>          
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" id="pauseIcon" viewBox="0 0 52 52"><path d="m30 43c0 1 0.9 2 2 2h4c1.1 0 2-1.1 2-2v-34c0-1-0.9-2-2-2h-4c-1.1 0-2 1.1-2 2v34z m-16 0c0 1 0.9 2 2 2h4c1.1 0 2-1.1 2-2v-34c0-1-0.9-2-2-2h-4c-1.1 0-2 1.1-2 2v34z"></path></svg>
+        </x-button>
       `;
       explorerContainer.append(playButtonTemplate.content);
     }
@@ -55,8 +60,21 @@ export default class XCarouselSlideshow extends CarouselSlideshow {
           }
 
           #playButton {
+            background: white;
             bottom: 0;
+            color: #706e6b;
+            height: 1.25rem;
+            line-height: 1;
+            line-height: 1;
             position: absolute;
+            vertical-align: middle;
+            width: 1.25rem;
+          }
+
+          .icon {
+            fill: currentColor;
+            height: 1rem;
+            width: 1rem;
           }
         </style>
       `
