2af3e34d8dfc9d1df45a4c0cee593ecde3e78ba2
Jan Miksovsky <jan@miksovsky.com>
2019-12-20 16:24:06 -0800

    Turn off generic MenuButton styling for the SLDS ButtonMenu.

--- src/ButtonMenu.js
+++ src/ButtonMenu.js
@@ -1,13 +1,14 @@
 import * as internal from "../node_modules/elix/src/internal.js";
 import * as template from "../node_modules/elix/src/template.js";
-import XButton from './Button.js';
-import XMenu from './Menu.js';
+import XButton from "./Button.js";
+import XMenu from "./Menu.js";
 import MenuButton from "../node_modules/elix/src/MenuButton.js";
 import XPopup from "./Popup.js";
 
 export default class XButtonMenu extends MenuButton {
   get [internal.defaultState]() {
     return Object.assign(super[internal.defaultState], {
+      generic: false,
       popupPartType: XPopup,
       menuPartType: XMenu,
       sourcePartType: XButton
@@ -23,7 +24,7 @@ export default class XButtonMenu extends MenuButton {
       const downIconTemplate = template.html`
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="downIcon"><path d="M3.8 6.5h16.4c.4 0 .8.6.4 1l-8 9.8c-.3.3-.9.3-1.2 0l-8-9.8c-.4-.4-.1-1 .4-1z"></path></svg>
       `;
-      sourceSlot.innerHTML = '';
+      sourceSlot.innerHTML = "";
       sourceSlot.append(downIconTemplate.content);
     }
 
